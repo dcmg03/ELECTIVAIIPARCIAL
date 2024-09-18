9f995625ab89b0eadf57baf43e271816b35ab52c
DeisyMonroy <deisy.monroy@uptc.edu.co>
2024-09-17 19:34:30 -0500

    Agrgar carros

--- routes/index.js
+++ routes/index.js
@@ -6,19 +6,23 @@ const router = express.Router();
 // Rutas de los archivos JSON
 const departmentsFilePath = path.join(__dirname, '../public/js/departments.json');
 const townsFilePath = path.join(__dirname, '../public/js/towns.json');
+const carsFilePath = path.join(__dirname, '../public/js/carros.json');
 
-// Leer y parsear datos de departamentos y municipios
+// Leer y parsear datos de departamentos, municipios y carros
 let departamentos = [];
 let municipios = {};
+let carros = [];
 
 // Función para cargar los datos desde los archivos JSON
 function loadJSONData() {
   try {
     const departmentsData = fs.readFileSync(departmentsFilePath, 'utf8');
     const townsData = fs.readFileSync(townsFilePath, 'utf8');
+    const carsData = fs.readFileSync(carsFilePath, 'utf8'); // Cargar carros
 
     departamentos = JSON.parse(departmentsData); // Cargar departamentos
     const towns = JSON.parse(townsData); // Cargar municipios
+    carros = JSON.parse(carsData); // Cargar carros
 
     // Organizar municipios por departamento
     towns.forEach(town => {
@@ -41,8 +45,8 @@ let objetosRegistrados = [];
 
 // Ruta para la página principal
 router.get('/', (req, res) => {
-  // Renderiza la vista 'index' con los departamentos y objetos registrados
-  res.render('index', { departamentos, objetos: objetosRegistrados });
+  // Renderiza la vista 'index' con los departamentos, carros y objetos registrados
+  res.render('index', { departamentos, carros, objetos: objetosRegistrados });
 });
 
 // Ruta para manejar la obtención de municipios basados en el departamento seleccionado
@@ -54,10 +58,10 @@ router.post('/get-municipios', (req, res) => {
 
 // Ruta para manejar la adición de un nuevo objeto
 router.post('/add-object', (req, res) => {
-  const { departamento, municipio, nombreObjeto } = req.body;
+  const { departamento, municipio, carro } = req.body; // Cambiar nombreObjeto a carro
 
   // Agrega el nuevo objeto a la lista
-  objetosRegistrados.push({ departamento, municipio, nombreObjeto });
+  objetosRegistrados.push({ departamento, municipio, carro });
 
   // Redirige a la página principal para mostrar el objeto agregado
   res.redirect('/');
